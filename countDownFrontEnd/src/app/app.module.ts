0f1787107e7c9be9d6046489a445550af7ef0730
dantehargrow <dantehargrow@gmail.com>
2021-01-30 01:39:53 -0600

    Imported routing module and new components

--- countDownFrontEnd/src/app/app.module.ts
+++ countDownFrontEnd/src/app/app.module.ts
@@ -2,13 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RoutingModule } from './routing/routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MoviesComponent } from './movies/movies.component';
+import { BooksComponent } from './books/books.component';
+import { MusicComponent } from './music/music.component';
+import { EventsComponent } from './events/events.component';
+import { GamesComponent } from './games/games.component';
+import { SettingsComponent } from './settings/settings.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    MoviesComponent,
+    BooksComponent,
+    MusicComponent,
+    EventsComponent,
+    GamesComponent,
+    SettingsComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
