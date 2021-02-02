e3dfe17012a3a94571556bbef777f79b2bcb5bfc
dantehargrow <dantehargrow@gmail.com>
2021-02-01 22:00:27 -0600

    on init call get movies function and loop through results to get titles

--- countDownFrontEnd/src/app/dashboard/dashboard.component.ts
+++ countDownFrontEnd/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  movies; //array of movie objects
+  title; //title of that movie
+  theMovie // single movie object
 
-  constructor() { }
+  constructor(private apiService: ApiService) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.apiService.get_movies().subscribe((data)=>{
+      this.movies = data['results'];
+      for(const movie in this.movies){
+        this.theMovie = this.movies[movie];
+        this.title = this.theMovie["original_title"];
+        console.log(this.title);
+
+      }
+      
+
+    });
   }
 
 }
