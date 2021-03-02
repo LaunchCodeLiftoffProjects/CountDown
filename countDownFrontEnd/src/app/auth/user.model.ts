08f6ef5fff14435ad4e4c19d9dd32d8e3d9f942f
Queue <elbertq2@gmail.com>
2021-03-01 21:44:20 -0600

    corrected posting to product database

--- countDownFrontEnd/src/app/auth/user.model.ts
+++ countDownFrontEnd/src/app/auth/user.model.ts
@@ -2,17 +2,20 @@
 // model stores all the data that makes up a user, and checks token validation
 export class User {
   constructor(
-    public email: string,
-    public id: string,
-    private _token: string,
-    private _tokenExpirationDate: Date
+    public name: string,
+    private _token: string
+    // public email: string,
+    // public id: string,
+    // private _token: string,
+    // private _tokenExpirationDate: Date
   ) {}
 
   // allows app to get the token and check its validity
   get token() {
     // checks to see if a token exists or if it does exist, it checks it against
     // the current date/time to make sure it is still valid
-    if (!this._tokenExpirationDate || new Date() > this._tokenExpirationDate) {
+    // if (!this._tokenExpirationDate || new Date() > this._tokenExpirationDate) {
+    if (!this._token) {
       return null;
     }
     return this._token;
